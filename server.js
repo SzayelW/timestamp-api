862849c61f3a1610fc5a8b3edcbfa5e36d13b8fe
Andres <andres70p32@gmail.com>
2017-04-08 23:43:20 +0000

    project working

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/',(req,res)=>{
+    res.sendfile('./index.html', {root: __dirname })
+});
+
+app.get('/:date', function (req, res) {
+    var stringDate = setParamDate(req.params.date)
+    res.send(JSON.stringify(stringDate))
 })
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
+
+function setParamDate(date){
+    var nDate
+    var nan = isNaN(date)
+    !nan ? nDate = new Date(Number.parseInt(date)) : nDate= new Date(Date.parse(date))
+    return getDateObject(nDate)
+}
+
+function getDateObject(date){
+    if(date == "Invalid Date"){
+      return {unix: "null", natural: "null"}  
+    } 
+    
+    var Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Octuber', 'November', 'December']
+    var unixD = date.getTime() 
+    var month = date.getUTCMonth()
+    var day = date.getDate()
+    var year = date.getFullYear()
+    
+    return {unix: unixD,
+        natural: Months[month] +' '+day+', '+year
+    }
+}
